d12584444af23885ea56d92986ba332e0cc4683c
Dimka <141173731+Dmitr1yy@users.noreply.github.com>
2023-08-09 04:18:37 +0400

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -6,7 +6,17 @@ interface AssertionErrorConstructor {
     new<T = {}>(message: string, props?: T, ssf?: Function): AssertionError<T>;
 }
 
+declare const AssertionError: AssertionErrorConstructor;type AssertionError<T = {}> = Error & T & {
+    showDiff: boolean;
+};
+
+interface AssertionErrorConstructor {
+    new<T = {}>(message: string, props?: T, ssf?: Function): AssertionError<T>;
+}
+
 declare const AssertionError: AssertionErrorConstructor;
+
+export = AssertionError;
 
 export = AssertionError;type AssertionError<T = {}> = Error & T & {
     showDiff: boolean;
